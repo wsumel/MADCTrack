3abecb45565367920e2740d38ff0b445dfdf718e
Slay <32010097+wsumel@users.noreply.github.com>
2026-03-09 17:59:17 +0800

    Update

--- run_test.py
+++ run_test.py
@@ -1,4 +1,5 @@
 import os
+# os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 import glob
 import argparse
 import shutil
@@ -53,6 +54,8 @@ def run_sequence(dir_path, file_extension, output_dir,init_box):
     # Track frame-by-frame
     print('Segmenting frames...')
     for i in range(len(frames_dir)):
+        import time
+        st = time.time()
         img = Image.open(frames_dir[i])
         img_vis = np.array(img)
 
@@ -60,7 +63,10 @@ def run_sequence(dir_path, file_extension, output_dir,init_box):
             outputs = tracker.initialize(img, None, bbox=init_box)
         else:
             outputs = tracker.track(img)
+        ed = time.time()
+        print("running time: ", ed-st)
         pred_mask = outputs['pred_mask']
+        
 
 
         pred_bbox = mask2box(pred_mask)
